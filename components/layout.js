a4f1e330c58cb5328aa0cda2a8435dd6dfac6d93
andriyfm <andrifmsyh@gmail.com>
2020-10-18 13:39:00 +0700

    Polishing Layout

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,65 @@
+import Head from "next/head";
 import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
 
-export default function Layout({ children }) {
-  return <div className={styles.container}>{children}</div>;
+const name = "Simple";
+
+export const siteTitle = "Next.js Simple Website";
+
+export default function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Learn how ot build a personal website using Next.js"
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <img
+              src="/images/turing-logo.png"
+              className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
+              alt={name}
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          <>
+            <Link href="/">
+              <a>
+                <img
+                  src="/images/turing-logo.png"
+                  alt={name}
+                  className={`${styles.headerImage} ${utilStyles.borderCircle}`}
+                />
+              </a>
+            </Link>
+
+            <h2 className={utilStyles.heading}>
+              <Link href="/">
+                <a className={utilStyles.colorInherit}>{name}</a>
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
+
+      <main>{children}</main>
+
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">
+            <a>⬅ Back to home</a>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
 }
