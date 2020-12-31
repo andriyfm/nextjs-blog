c5ec694bf1fc95d4243b29b3f3942cdfa645b159
andriyfm <andrifmsyh@gmail.com>
2021-01-01 00:39:13 +0700

    Finishing the basic of dynamic routes & api routes

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
-import { getSortedPostsData } from "../lib/post";
 import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
 
 export default function Home({ allPostsData }) {
   return (
@@ -9,32 +11,24 @@ export default function Home({ allPostsData }) {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-
       <section className={utilStyles.headingMd}>
-        <p>
-          Occaecat cupidatat sint veniam ut Lorem nulla labore pariatur quis
-          consectetur dolore. In duis in nostrud dolor. Excepteur reprehenderit
-          excepteur excepteur do ad ut. Duis aliquip est ea consequat incididunt
-          dolor qui velit officia aliqua quis adipisicing nostrud.
-        </p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <p>[Your Self Introduction]</p>
       </section>
-
-      <section>
-        {allPostsData.map(({ id, date, title }) => {
-          return (
-            <li key={id}>
-              {title}
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
-          );
-        })}
+          ))}
+        </ul>
       </section>
     </Layout>
   );
@@ -42,5 +36,9 @@ export default function Home({ allPostsData }) {
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
-  return { props: { allPostsData } };
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
